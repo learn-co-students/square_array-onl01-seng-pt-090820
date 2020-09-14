0214fb55611d43056222e39c18730a28376742ff
Carl J <carlj1881@yahoo.com>
2020-09-14 22:52:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  numbers_array = []
+  numbers.each do |number|
+  numbers_new = number * number
+  numbers_array.push(numbers_new)
+  puts "The number squared is #{numbers_new}"
+  end
+  return numbers_array
 end
