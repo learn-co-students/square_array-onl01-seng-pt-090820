da08550f9d86ed4edfaaa04f09335717aa00a3e8
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-16 18:11:17 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(square)
   square.each do |number|
-    new_array = (number*number)
+    new_array = [number*number]
       puts "The square of number {number} is #{number*number}"
 end 
 end 
