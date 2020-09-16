2cab1c07725cb41a9105ade836d8679d3749f046
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-16 18:19:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
-def square_array(square)
+def square_array(square)   
+  new_array = []
+  
   square.each do |number|
-    new_array = [number*number]
-      puts "The square of number {number} is #{number*number}"
+ new_array = new_array << number*number
+      puts "The square of number {number} is new_array"
+      
 end 
 end 
