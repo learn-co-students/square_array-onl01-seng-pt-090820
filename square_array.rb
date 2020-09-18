30059ae103e16f5768f0a4961030e8a9edec56db
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-18 22:26:23 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+
+
+def square_array(numbers)
+  exponent_number = []
+  numbers.each do |number|
+    exponent_number.push(number ** 2)
+  end
+(exponent_number)
 end
